5591272babe8f3032e7918226d5e705630e55e1f
DPR-Sanchez <david.p.sanchez@enmu.edu>
2019-07-10 23:08:54 -0600

    Initial GUI mock of DeepWatch photo model.

---
+++ GUI.py
@@ -0,0 +1,37 @@
+import PySimpleGUI as sg
+
+character_roster = ('Tracer','Bastion','Hanzo','Genji','McCree','Reaper','D.va','Soldier76','DoomFist','Hammond','Rheinhart','Winston',
+					'Baptiste','Brigette','Torjorn','Anna','Mei','Mercy','Lucio','Orisa','Junkrat','Roadhog','Ashe','Moira','Phara',
+					'Sombra','Widow Maker','Symmetra','Zarya','Zenyatta')
+level_of_play = ['Bronze','Silver','Gold','Platinum','Diamond','Master','Grand Master']
+map = ['Hanamura']
+sg.ChangeLookAndFeel('Black')
+
+column1 = [[sg.Text('Column 1', background_color='#d3dfda', justification='center', size=(10, 1))],
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 1')],
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 2')],
+           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
+layout = [
+    [sg.Text('DeepWatch Interface', size=(30, 1), font=("Helvetica", 25))],
+    [sg.Text('Your team\t\tEnemy Team')],
+
+
+    [sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.Text('Map:')],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.InputCombo(map, size=(20, 3))],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.Text('Level of Play:')],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.InputCombo(level_of_play, size=(20, 3))],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3))],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3))],
+	[sg.T(' ')],
+	[sg.T(' '*10),sg.Button('Predict'),sg.T(' '*30),sg.Text('Label Data for Training:')],
+	[sg.T(' '*60),sg.InputCombo(('Loss'), size=(20, 3))],
+	[sg.T(' '*60),sg.Button('Add example')],
+    [sg.Text('_'  * 80)],
+    [sg.Text('Choose A Folder', size=(35, 1))],
+    [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
+     sg.InputText('Default Folder'), sg.FolderBrowse()],
+    [sg.Submit(), sg.Cancel()]
+]
+
+window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(layout)
+button, values = window.Read()
