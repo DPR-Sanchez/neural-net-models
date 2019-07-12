f1730330569a1715acc4e69526551a83b1483036
DPR-Sanchez <david.p.sanchez@enmu.edu>
2019-07-12 09:52:46 -0600

    added launch mode selection window as well as an exit value flag so all event loops are stopped on exit signal.

--- GUI.py
+++ GUI.py
@@ -7,16 +7,17 @@ level_of_play = ['Bronze','Silver','Gold','Platinum','Diamond','Master','Grand M
 map = ['Hanamura']
 sg.ChangeLookAndFeel('Black')
 
-column1 = [[sg.Text('Column 1', background_color='#d3dfda', justification='center', size=(10, 1))],
-           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 1')],
-           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 2')],
-           [sg.Spin(values=('Spin Box 1', '2', '3'), initial_value='Spin Box 3')]]
+main_menu_layout = [
+						[sg.Button('General Training'),sg.Button('DeepWatch')],
+						[sg.T(' '*10)],
+						[sg.Button('Exit')]
+					]
+
+
 deepwatch_layout = [
-    [sg.Text('DeepWatch Interface', size=(30, 1), font=("Helvetica", 25))],
-    [sg.Text('Your team\t\tEnemy Team')],
-
-
-    [sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.Text('Map:')],
+	[sg.Text('DeepWatch Interface', size=(30, 1), font=("Helvetica", 25))],
+	[sg.Text('Your team\t\tEnemy Team')],
+	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.Text('Map:')],
 	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.InputCombo(map, size=(20, 3))],
 	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.Text('Level of Play:')],
 	[sg.InputCombo(character_roster, size=(20, 3)),sg.InputCombo(character_roster, size=(20, 3)),sg.T(' '  * 10),sg.InputCombo(level_of_play, size=(20, 3))],
@@ -26,25 +27,36 @@ deepwatch_layout = [
 	[sg.T(' '*10),sg.Button('Predict'),sg.T(' '*30),sg.Text('Label Data for Training:')],
 	[sg.T(' '*60),sg.InputCombo(('Loss'), size=(20, 3))],
 	[sg.T(' '*60),sg.Button('Add example')],
-    [sg.Text('_'  * 80)],
-    [sg.Text('Choose A Folder', size=(35, 1))],
+	[sg.Text('_'  * 80)],
+	[sg.Text('Choose A Folder', size=(35, 1))],
     [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
      sg.InputText('Default Folder'), sg.FolderBrowse()],
-    [sg.Submit(), sg.Cancel()]
+    [sg.Submit(), sg.Button('Exit')]
 ]
 
 selection_tuple = ()
 hero_index = {character_roster[x]:x for x in range(0,len(character_roster))}
 
-deepwatch_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(deepwatch_layout)
+main_menu_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(main_menu_layout)
 
-while True:
-	event, values = deepwatch_window.Read()
+exit_value = False
+
+while not exit_value:
+	event, values = main_menu_window.Read()
 
 	if event is None or event == 'Exit':
-		break
-	elif event == 'Predict':
-		heros_selected = [hero_index[hero] for hero in [values[x] for x in selection_tuple]]
+		exit_value = True
+	elif event == 'DeepWatch':
+		main_menu_window.Close()
+		deepwatch_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(deepwatch_layout)
+
+		event, values = deepwatch_window.Read()
+
+		while not exit_value:
+			if event is None or event == 'Exit':
+				exit_value = True
+			elif event == 'Predict':
+				heros_selected = [hero_index[hero] for hero in [values[x] for x in selection_tuple]]
 
 	print(event, values)
 
