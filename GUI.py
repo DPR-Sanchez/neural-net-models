8699f0c4b7ff8306deafc023ca8783808b55e4e8
DPR-Sanchez <david.p.sanchez@enmu.edu>
2019-07-20 19:12:08 -0600

    reducing gui_layout overhead.

--- GUI.py
+++ GUI.py
@@ -7,79 +7,86 @@ import PySimpleGUI as sg
 import asyncio		
 
 
-async def gui_layouts():
-
+async def gui_layouts(layout):
 	sg.ChangeLookAndFeel('Black')
-	main_menu_layout = [
-		[sg.Button('General Training'), sg.Button('DeepWatch')],
-		[sg.T(' ' * 10)],
-		[sg.Button('Exit')]
-	]
-	
-	NETWORK_MODELS, COST_FUNCTION_NAMES, COST_FUNCTION_VALUES  = CONSTANTS.NEUR_CONSTANTS().get_constants_tuple()
-	general_training_layout = [
-		[sg.T(' ')],
-		[sg.Button('Predict'), sg.Button('Train'),sg.Button('Show training history'),
-		 sg.Text('Cost function for Training:')],
-		[sg.T(' ' * 60), sg.InputCombo(COST_FUNCTION_NAMES, size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('net model:')],
-		[sg.T(' ' * 60), sg.InputCombo(NETWORK_MODELS, size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('epoch training period:')],
-		[sg.T(' ' * 60), sg.Input(default_text='100', size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('numpy seed:')],
-		[sg.T(' ' * 60), sg.Input(default_text='614', size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('tensorflow  seed:')],
-		[sg.T(' ' * 60), sg.Input(default_text='1234', size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('rand seed:')],
-		[sg.T(' ' * 60), sg.Input(default_text='2', size=(20, 3))],
-		[sg.T(' ' * 60), sg.Text('uploaded dataset: ')],
-		[sg.Text('_' * 80)],
-		[sg.Text('Choose your desired dataset that you would like to predict or train on', size=(60, 1))],
-		[sg.Text('Dataset:', size=(15, 1), auto_size_text=False, justification='right'),
-		 sg.InputText('Select dataset >>'), sg.FileBrowse()],
-		[sg.Text('select a trained neural network you would like to load', size=(60, 1))],
-		[sg.Text('trained net:', size=(15, 1), auto_size_text=False, justification='right'),
-		 sg.InputText('Select trained neural net >>'), sg.FileBrowse()],
-		[sg.Text('Save trained network:', size=(35, 1))],
-		[sg.Text('save location:', size=(15, 1), auto_size_text=False, justification='right'),
-		 sg.InputText('Select save location >>'), sg.FileSaveAs()],
-		[sg.Submit(), sg.Button('Load Net'), sg.Button('Save Net'), sg.Button('Exit')]
-	]
 
-	character_roster, level_of_play, map = CONSTANTS.DEEPWATCH_CONSTANTS().get_constants_tuple()
+	if layout == 'main menu':
 
-	deepwatch_layout = [
-		[sg.Text('DeepWatch Interface', size=(30, 1), font=("Helvetica", 25))],
-		[sg.Text('Your team\t\tEnemy Team')],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
-		 sg.T(' ' * 10), sg.Text('Map:')],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
-		 sg.T(' ' * 10), sg.InputCombo(map, size=(20, 3))],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
-		 sg.T(' ' * 10), sg.Text('Level of Play:')],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
-		 sg.T(' ' * 10), sg.InputCombo(level_of_play, size=(20, 3))],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3))],
-		[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3))],
-		[sg.T(' ')],
-		[sg.T(' ' * 10), sg.Button('Predict'), sg.T(' ' * 30), sg.Text('Label Data for Training:')],
-		[sg.T(' ' * 10), sg.Text('Result:'), sg.T(' ' * 33), sg.InputCombo(('Loss'), size=(20, 3))],
-		[sg.T(' ' * 8), sg.Text('', size=(10, 1), font=("Helvetica", 25), key='change'),
-		 sg.Button('Add example')],
-		[sg.Text('_' * 80)],
-		[sg.Text('Choose A Folder', size=(35, 1))],
-		[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
-		 sg.InputText('Selet file >>'), sg.FileBrowse()],
-		[sg.Submit(), sg.Button('Exit')]
-	]
+		main_menu_layout = [
+			[sg.Button('General Training'), sg.Button('DeepWatch')],
+			[sg.T(' ' * 10)],
+			[sg.Button('Exit')]
+		]
+		return main_menu_layout
+	if layout == 'general':
+		NETWORK_MODELS, COST_FUNCTION_NAMES, COST_FUNCTION_VALUES  = CONSTANTS.NEUR_CONSTANTS().get_constants_tuple()
+		general_training_layout = [
+			[sg.T(' ')],
+			[sg.Button('Predict'), sg.Button('Train'),sg.Button('Show training history'),
+			 sg.Text('Cost function for Training:')],
+			[sg.T(' ' * 60), sg.InputCombo(COST_FUNCTION_NAMES, size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('net model:')],
+			[sg.T(' ' * 60), sg.InputCombo(NETWORK_MODELS, size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('epoch training period:')],
+			[sg.T(' ' * 60), sg.Input(default_text='100', size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('numpy seed:')],
+			[sg.T(' ' * 60), sg.Input(default_text='614', size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('tensorflow  seed:')],
+			[sg.T(' ' * 60), sg.Input(default_text='1234', size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('rand seed:')],
+			[sg.T(' ' * 60), sg.Input(default_text='2', size=(20, 3))],
+			[sg.T(' ' * 60), sg.Text('uploaded dataset: ')],
+			[sg.Text('_' * 80)],
+			[sg.Text('Choose your desired dataset that you would like to predict or train on', size=(60, 1))],
+			[sg.Text('Dataset:', size=(15, 1), auto_size_text=False, justification='right'),
+			 sg.InputText('Select dataset >>'), sg.FileBrowse()],
+			[sg.Text('select a trained neural network you would like to load', size=(60, 1))],
+			[sg.Text('trained net:', size=(15, 1), auto_size_text=False, justification='right'),
+			 sg.InputText('Select trained neural net >>'), sg.FileBrowse()],
+			[sg.Text('Save trained network:', size=(35, 1))],
+			[sg.Text('save location:', size=(15, 1), auto_size_text=False, justification='right'),
+			 sg.InputText('Select save location >>'), sg.FileSaveAs()],
+			[sg.Submit(), sg.Button('Load Net'), sg.Button('Save Net'), sg.Button('Exit')]
+		]
+		return general_training_layout
 
-	return [main_menu_layout,general_training_layout, deepwatch_layout]
+	if layout == 'deep watch':
+		character_roster, level_of_play, map = CONSTANTS.DEEPWATCH_CONSTANTS().get_constants_tuple()
+
+		deepwatch_layout = [
+			[sg.Text('DeepWatch Interface', size=(30, 1), font=("Helvetica", 25))],
+			[sg.Text('Your team\t\tEnemy Team')],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
+			 sg.T(' ' * 10), sg.Text('Map:')],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
+			 sg.T(' ' * 10), sg.InputCombo(map, size=(20, 3))],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
+			 sg.T(' ' * 10), sg.Text('Level of Play:')],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3)),
+			 sg.T(' ' * 10), sg.InputCombo(level_of_play, size=(20, 3))],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3))],
+			[sg.InputCombo(character_roster, size=(20, 3)), sg.InputCombo(character_roster, size=(20, 3))],
+			[sg.T(' ')],
+			[sg.T(' ' * 10), sg.Button('Predict'), sg.T(' ' * 30), sg.Text('Label Data for Training:')],
+			[sg.T(' ' * 10), sg.Text('Result:'), sg.T(' ' * 33), sg.InputCombo(('Loss'), size=(20, 3))],
+			[sg.T(' ' * 8), sg.Text('', size=(10, 1), font=("Helvetica", 25), key='change'),
+			 sg.Button('Add example')],
+			[sg.Text('_' * 80)],
+			[sg.Text('Choose A Folder', size=(35, 1))],
+			[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),
+			 sg.InputText('Selet file >>'), sg.FileBrowse()],
+			[sg.Submit(), sg.Button('Exit')]
+		]
+
+		return deepwatch_layout
+
+	return None
 
 
 
 async def events_loop(layouts_list):
 	exit_value = False
-	main_menu_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(layouts_list[0])
+	main_menu_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(await gui_layouts('main menu'))
 
 	while not exit_value:
 		event, values = main_menu_window.Read()
@@ -94,8 +101,7 @@ async def events_loop(layouts_list):
 
 
 			main_menu_window.Close()
-			deepwatch_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(
-				layouts_list[2])
+			deepwatch_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(await gui_layouts('deep watch'))
 
 			character_roster, level_of_play, map = CONSTANTS.DEEPWATCH_CONSTANTS().get_constants_tuple()
 			
@@ -120,7 +126,7 @@ async def events_loop(layouts_list):
 		elif event == 'General Training':
 			main_menu_window.Close()
 			general_training_window = sg.Window('Neur - A Net Creation Tool', default_element_size=(40, 1)).Layout(
-				layouts_list[1])
+				await gui_layouts('general'))
 
 			trained_net = None
 
